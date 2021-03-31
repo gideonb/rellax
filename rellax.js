d8b79472e43442909d3a7a9533cc5ebfbb353c71
Ivan Ivanov <reinosko@gmail.com>
2021-03-31 18:52:55 +0300

    rellax frame based speed

--- rellax.js
+++ rellax.js
@@ -1,4 +1,3 @@
-
 // ------------------------------------------
 // Rellax.js
 // Buttery smooth parallax library
@@ -22,9 +21,9 @@
     // Browser globals (root is window)
     root.Rellax = factory();
   }
-}(typeof window !== "undefined" ? window : global, function () {
-  var Rellax = function(el, options){
-    "use strict";
+})(typeof window !== 'undefined' ? window : global, function () {
+  var Rellax = function (el, options) {
+    'use strict';
 
     var self = Object.create(Rellax.prototype);
 
@@ -37,12 +36,15 @@
 
     // check what requestAnimationFrame to use, and if
     // it's not supported, use the onscroll event
-    var loop = window.requestAnimationFrame ||
+    var loop =
+      window.requestAnimationFrame ||
       window.webkitRequestAnimationFrame ||
       window.mozRequestAnimationFrame ||
       window.msRequestAnimationFrame ||
       window.oRequestAnimationFrame ||
-      function(callback){ return setTimeout(callback, 1000 / 60); };
+      function (callback) {
+        return setTimeout(callback, 1000 / 60);
+      };
 
     // store the id for later use
     var loopId = null;
@@ -51,24 +53,26 @@
     var supportsPassive = false;
     try {
       var opts = Object.defineProperty({}, 'passive', {
-        get: function() {
+        get: function () {
           supportsPassive = true;
-        }
+        },
       });
-      window.addEventListener("testPassive", null, opts);
-      window.removeEventListener("testPassive", null, opts);
+      window.addEventListener('testPassive', null, opts);
+      window.removeEventListener('testPassive', null, opts);
     } catch (e) {}
 
     // check what cancelAnimation method to use
     var clearLoop = window.cancelAnimationFrame || window.mozCancelAnimationFrame || clearTimeout;
 
     // check which transform property to use
-    var transformProp = window.transformProp || (function(){
+    var transformProp =
+      window.transformProp ||
+      (function () {
         var testEl = document.createElement('div');
         if (testEl.style.transform === null) {
           var vendors = ['Webkit', 'Moz', 'ms'];
           for (var vendor in vendors) {
-            if (testEl.style[ vendors[vendor] + 'Transform' ] !== undefined) {
+            if (testEl.style[vendors[vendor] + 'Transform'] !== undefined) {
               return vendors[vendor] + 'Transform';
             }
           }
@@ -86,12 +90,13 @@
       vertical: true,
       frame: null,
       horizontal: false,
-      callback: function() {},
+      aspectRatio: 1,
+      callback: function () {},
     };
 
     // User defined options (might have more in the future)
-    if (options){
-      Object.keys(options).forEach(function(key){
+    if (options) {
+      Object.keys(options).forEach(function (key) {
         self.options[key] = options[key];
       });
     }
@@ -143,20 +148,18 @@
       }
     }
 
-
     // Get and cache initial position of all elements
-    var cacheBlocks = function() {
-      for (var i = 0; i < self.elems.length; i++){
+    var cacheBlocks = function () {
+      for (var i = 0; i < self.elems.length; i++) {
         var block = createBlock(self.elems[i]);
         blocks.push(block);
       }
     };
 
-
     // Let's kick this script off
     // Build array for cached element values
-    var init = function() {
-      for (var i = 0; i < blocks.length; i++){
+    var init = function () {
+      for (var i = 0; i < blocks.length; i++) {
         self.elems[i].style.cssText = blocks[i].style;
       }
 
@@ -164,6 +167,7 @@
 
       screenY = window.innerHeight;
       screenX = window.innerWidth;
+
       setPosition();
 
       cacheBlocks();
@@ -182,26 +186,32 @@
     // We want to cache the parallax blocks'
     // values: base, top, height, speed
     // el: is dom object, return: el cache values
-    var createBlock = function(el) {
-      var dataPercentage = el.getAttribute( 'data-rellax-percentage' );
-      var dataSpeed = el.getAttribute( 'data-rellax-speed' );
-      var dataZindex = el.getAttribute( 'data-rellax-zindex' ) || 0;
-      var dataMin = el.getAttribute( 'data-rellax-min' );
-      var dataMax = el.getAttribute( 'data-rellax-max' );
+    var createBlock = function (el) {
+      var dataPercentage = el.getAttribute('data-rellax-percentage');
+      var dataSpeed = el.getAttribute('data-rellax-speed');
+      var dataZindex = el.getAttribute('data-rellax-zindex') || 0;
+      var dataMin = el.getAttribute('data-rellax-min');
+      var dataMax = el.getAttribute('data-rellax-max');
 
       // initializing at scrollY = 0 (top of browser), scrollX = 0 (left of browser)
       // ensures elements are positioned based on HTML layout.
       //
       // If the element has the percentage attribute, the posY and posX needs to be
       // the current scroll position's value, so that the elements are still positioned based on HTML layout
-      var wrapperPosY = self.options.wrapper ? self.options.wrapper.scrollTop : (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop);
+      var wrapperPosY = self.options.wrapper ? self.options.wrapper.scrollTop : window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
       // If the option relativeToWrapper is true, use the wrappers offset to top, subtracted from the current page scroll.
       if (self.options.relativeToWrapper) {
-        var scrollPosY = (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop);
+        var scrollPosY = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
         wrapperPosY = scrollPosY - self.options.wrapper.offsetTop;
       }
-      var posY = self.options.vertical ? ( dataPercentage || self.options.center ? wrapperPosY : 0 ) : 0;
-      var posX = self.options.horizontal ? ( dataPercentage || self.options.center ? self.options.wrapper ? self.options.wrapper.scrollLeft : (window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft) : 0 ) : 0;
+      var posY = self.options.vertical ? (dataPercentage || self.options.center ? wrapperPosY : 0) : 0;
+      var posX = self.options.horizontal
+        ? dataPercentage || self.options.center
+          ? self.options.wrapper
+            ? self.options.wrapper.scrollLeft
+            : window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft
+          : 0
+        : 0;
 
       var blockTop = posY + el.getBoundingClientRect().top;
       var blockHeight = el.clientHeight || el.offsetHeight || el.scrollHeight;
@@ -212,16 +222,21 @@
       // apparently parallax equation everyone uses
       var percentageY = dataPercentage ? dataPercentage : (posY - blockTop + screenY) / (blockHeight + screenY);
       var percentageX = dataPercentage ? dataPercentage : (posX - blockLeft + screenX) / (blockWidth + screenX);
-      if(self.options.center){ percentageX = 0.5; percentageY = 0.5; }
+      if (self.options.center) {
+        percentageX = 0.5;
+        percentageY = 0.5;
+      }
 
       // Optional individual block speed as data attr, otherwise global speed
       var speed = dataSpeed ? dataSpeed : self.options.speed;
 
       if (self.options.frame) {
-        var frame = self.options.frame
+        var frame = self.options.frame;
         var frameHeight = frame.clientHeight || frame.offsetHeight || frame.scrollHeight;
         var overlap = blockHeight - frameHeight;
-        speed = overlap/100;
+        var aspectRatio = self.options.aspectRatio;
+
+        speed = (overlap * aspectRatio) / 100;
       }
 
       var bases = updatePosition(percentageX, percentageY, speed);
@@ -243,9 +258,9 @@
 
         // Remove "transform" string and save the attribute
         if (delimiter) {
-          transform = " " + trimmedStyle.slice(11, delimiter).replace(/\s/g,'');
+          transform = ' ' + trimmedStyle.slice(11, delimiter).replace(/\s/g, '');
         } else {
-          transform = " " + trimmedStyle.slice(11).replace(/\s/g,'');
+          transform = ' ' + trimmedStyle.slice(11).replace(/\s/g, '');
         }
       }
 
@@ -261,14 +276,14 @@
         transform: transform,
         zindex: dataZindex,
         min: dataMin,
-        max: dataMax
+        max: dataMax,
       };
     };
 
     // set scroll position (posY, posX)
     // side effect method is not ideal, but okay for now
     // returns true if the scroll changed, false if nothing happened
-    var setPosition = function() {
+    var setPosition = function () {
       var oldY = posY;
       var oldX = posX;
 
@@ -279,7 +294,6 @@
         var scrollPosY = (document.documentElement || document.body.parentNode || document.body).scrollTop || window.pageYOffset;
         posY = scrollPosY - self.options.wrapper.offsetTop;
       }
-
 
       if (oldY != posY && self.options.vertical) {
         // scroll changed, return true
@@ -298,10 +312,10 @@
     // Ahh a pure function, gets new transform value
     // based on scrollPosition and speed
     // Allow for decimal pixel values
-    var updatePosition = function(percentageX, percentageY, speed) {
+    var updatePosition = function (percentageX, percentageY, speed) {
       var result = {};
-      var valueX = (speed * (100 * (1 - percentageX)));
-      var valueY = (speed * (100 * (1 - percentageY)));
+      var valueX = speed * (100 * (1 - percentageX));
+      var valueY = speed * (100 * (1 - percentageY));
 
       result.x = self.options.round ? Math.round(valueX) : Math.round(valueX * 100) / 100;
       result.y = self.options.round ? Math.round(valueY) : Math.round(valueY * 100) / 100;
@@ -310,7 +324,7 @@
     };
 
     // Remove event listeners and loop again
-    var deferredUpdate = function() {
+    var deferredUpdate = function () {
       window.removeEventListener('resize', deferredUpdate);
       window.removeEventListener('orientationchange', deferredUpdate);
       (self.options.wrapper ? self.options.wrapper : window).removeEventListener('scroll', deferredUpdate);
@@ -321,7 +335,7 @@
     };
 
     // Loop
-    var update = function() {
+    var update = function () {
       if (setPosition() && pause === false) {
         animate();
 
@@ -333,20 +347,28 @@
         // Don't animate until we get a position updating event
         window.addEventListener('resize', deferredUpdate);
         window.addEventListener('orientationchange', deferredUpdate);
-        (self.options.wrapper ? self.options.wrapper : window).addEventListener('scroll', deferredUpdate, supportsPassive ? { passive: true } : false);
-        (self.options.wrapper ? self.options.wrapper : document).addEventListener('touchmove', deferredUpdate, supportsPassive ? { passive: true } : false);
+        (self.options.wrapper ? self.options.wrapper : window).addEventListener('scroll', deferredUpdate, supportsPassive ? {passive: true} : false);
+        (self.options.wrapper ? self.options.wrapper : document).addEventListener('touchmove', deferredUpdate, supportsPassive ? {passive: true} : false);
       }
     };
 
     // Transform3d on parallax element
-    var animate = function() {
+    var animate = function () {
       var positions;
-      for (var i = 0; i < self.elems.length; i++){
-        var percentageY = ((posY - blocks[i].top + screenY) / (blocks[i].height + screenY));
-        var percentageX = ((posX - blocks[i].left + screenX) / (blocks[i].width + screenX));
+      for (var i = 0; i < self.elems.length; i++) {
+        var percentageY = (posY - blocks[i].top + screenY) / (blocks[i].height + screenY);
+        var percentageX = (posX - blocks[i].left + screenX) / (blocks[i].width + screenX);
+
+        // Stop animating when scroll past the element
+        var frameNode = self.elems[i].parentNode;
+        var frameHeight = frameNode.clientHeight || frameNode.offsetHeight || frameNode.scrollHeight;
+        var frameTop = frameNode.getBoundingClientRect().top + posY;
+        var frameBottom = frameHeight + frameTop;
+
+        if (frameBottom < posY || frameTop > posY + screenY) return;
 
         // Subtracting initialize value, so element stays in same spot as HTML
-        positions = updatePosition(percentageX, percentageY, blocks[i].speed);// - blocks[i].baseX;
+        positions = updatePosition(percentageX, percentageY, blocks[i].speed); // - blocks[i].baseX;
         var positionY = positions.y - blocks[i].baseY;
         var positionX = positions.x - blocks[i].baseX;
 
@@ -387,8 +409,8 @@
       self.options.callback(positions);
     };
 
-    self.destroy = function() {
-      for (var i = 0; i < self.elems.length; i++){
+    self.destroy = function () {
+      for (var i = 0; i < self.elems.length; i++) {
         self.elems[i].style.cssText = blocks[i].style;
       }
 
@@ -412,4 +434,4 @@
     return self;
   };
   return Rellax;
-}));
+});
